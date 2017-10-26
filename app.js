15134fbecae15332bd454900630e61b667fb4cd1
Gleidson Balcazar <gleidsonsbalcazar@gmail.com>
2017-10-25 22:27:29 -0300

    Adicionando CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
 var routes = require('./routes/routes.js')(app);
 
