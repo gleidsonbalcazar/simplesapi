6ee6ba8a451acc3fc3197e4a05d1623c0d231af6
Gleidson Balcazar <gleidsonsbalcazar@gmail.com>
2017-10-25 23:40:56 -0300

    Erro no folder

--- routes/routes.js
+++ routes/routes.js
@@ -8,20 +8,20 @@ var appRouter = function(app){
     } 
 
     var imagens = [
-      "assets/photos/2/jpg",
-      "assets/photos/1/jpg",
-      "assets/photos/3/jpg",
-      "assets/photos/4/jpg",
-      "assets/photos/5/jpg",
-      "assets/photos/6/jpg",
-      "assets/photos/7/jpg",
-      "assets/photos/8/jpg",
-      "assets/photos/9/jpg",
-      "assets/photos/10/jpg",
-      "assets/photos/11/jpg",
-      "assets/photos/12/jpg",
-      "assets/photos/13/jpg",
-      "assets/photos/14/jpg"
+      "assets/photos/2.jpg",
+      "assets/photos/1.jpg",
+      "assets/photos/3.jpg",
+      "assets/photos/4.jpg",
+      "assets/photos/5.jpg",
+      "assets/photos/6.jpg",
+      "assets/photos/7.jpg",
+      "assets/photos/8.jpg",
+      "assets/photos/9.jpg",
+      "assets/photos/10.jpg",
+      "assets/photos/11.jpg",
+      "assets/photos/12.jpg",
+      "assets/photos/13.jpg",
+      "assets/photos/14.jpg"
 
     ]
 
