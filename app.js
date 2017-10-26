67782bbdd7d2ca28dd3945141e0d5f8d3b2f24ce
Gleidson Balcazar <gleidsonsbalcazar@gmail.com>
2017-10-25 23:45:11 -0300

    Express Static 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
-app.use(express.static('assets'))
+app.use(express.static(__dirname + 'assets'))
 
 var routes = require('./routes/routes.js')(app);
 
